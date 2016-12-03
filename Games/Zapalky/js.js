a32e4fe5fe8d7fe41703ea568b355ec642db9e42
Matus Lukac <matus@MacBook-Pro-uzivatela-Matus.local>
2016-12-03 16:08:28 +0100

    EDIT: uprava zapalie-priadanie kol

--- Games/Zapalky/js.js
+++ Games/Zapalky/js.js
@@ -11,9 +11,9 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     var dragDiv = document.getElementById(data)
     var dropDiv = ev.target;
+    console.log(vysledky[1])
      console.log(dropDiv.childNodes[0])
-     create();
-    if(dropDiv.childNodes.length == 1){
+        if(dropDiv.childNodes.length == 1){
     var castZaplaky = dragDiv.childNodes[0];
     var w  = castZaplaky.style.width;
     var h = castZaplaky.style.height;
--- Games/Zapalky/js2.js
+++ Games/Zapalky/js2.js
@@ -1,26 +1,71 @@
+var pole = [["110px","10px","110px","120px","100px","10px"],
+			["110px","10px","230px","120px","100px","10px"],
+			["110px","10px","350px","120px","100px","10px"],
+			["110px","10px","110px","480px","100px","10px"],
+			["110px","10px","230px","480px","100px","10px"],
+			["110px","10px","350px","480px","100px","10px"],
+			["110px","10px","110px","840px","100px","10px"],
+			["110px","10px","230px","840px","100px","10px"],
+			["110px","10px","350px","840px","100px","10px"],
+			["10px","110px","120px","110px","10px","100px"],
+			["10px","110px","120px","230px","10px","100px"],
+			["10px","110px","120px","470px","10px","100px"],
+			["10px","110px","120px","590px","10px","100px"],
+			["10px","110px","120px","830px","10px","100px"],
+			["10px","110px","120px","950px","10px","100px"],
+			["10px","110px","240px","110px","10px","100px"],
+			["10px","110px","240px","230px","10px","100px"],
+			["10px","110px","240px","470px","10px","100px"],
+			["10px","110px","240px","590px","10px","100px"],
+			["10px","110px","240px","830px","10px","100px"],
+			["10px","110px","240px","950px","10px","100px"],
+			["10px","110px","180px","350px","10px","100px"],];
 
-id =1;
+var vysledky = [[1,3,4,5,6,7,8,9,10,11,13,15,16,17,19,20,22],
+			[1,2,3,7,8,9,10,13,14,15,17,19,20,21],
+			[1,2,3,4,6,7,8,9,10,11,12,13,14,17,18,19,21,22],
+			[1,2,3,4,8,11,13,14,15,17,19,21],
+			[1,2,3,4,5,6,7,8,9,10,12,13,14,16,17,19,21,22],
+			[1,2,3,4,5,6,7,8,9,10,12,14,15,17,19,20,21,22],
+			[1,3,4,5,6,7,8,9,10,11,13,14,15,16,17,19,21,22],
+			[1,2,3,4,6,7,8,9,11,12,13,15,17,18,19,20,22],
+			[1,2,3,4,5,6,7,8,9,10,11,12,14,16,17,18,19,20,21],
+			[4,6,7,11,12,13,15,17,18,19,21,22]];
+
+var kola = [[1,3,4,5,6,7,8,9,10,11,13,15,16,17,18,20,22],
+			[1,2,3,7,8,9,10,13,14,17,19,20,21,22],
+			[1,2,3,4,6,7,8,9,10,11,12,13,14,15,17,18,19,21],
+			[1,2,3,8,11,13,14,15,17,19,21,22],
+			[1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,19,21,22],
+			[1,2,3,4,5,6,7,8,9,10,13,14,15,17,19,20,21,22],
+			[1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,19,21,22],
+			[1,2,3,4,6,7,8,9,11,12,13,15,16,18,19,20,22],
+			[1,3,4,5,6,7,8,9,10,11,12,14,16,17,18,19,20,21,22],
+			[4,5,6,7,11,12,15,17,18,19,21,22]]
+ i =0;
+j= 7;
 function create(){
+	
+	for(i=0;i<kola[j].length;i++){
 	var iDiv = document.createElement('div');
-	if (id<4){
-	iDiv.id = id;
+	iDiv.id = i;
 	iDiv.className = 'zapalka';
-	iDiv.style.height = "110px";
-	iDiv.style.width = "10px";
-	iDiv.style.left = "10px";
-	iDiv.style.top = "10px";
+	iDiv.style.width = pole[kola[j][i]-1][0];
+	iDiv.style.height = pole[kola[j][i]-1][1];
+	iDiv.style.top = pole[kola[j][i]-1][2];
+	iDiv.style.left = pole[kola[j][i]-1][3];
 	iDiv.draggable = "true";
 	iDiv.ondragstart =drag;
 
 	// Now create and append to iDiv
 	var omg= document.createElement('div');
-	omg.style.width = "10px";
-	omg.style.height = "100px";
+	omg.style.width = pole[kola[j][i]-1][4];
+	omg.style.height = pole[kola[j][i]-1][5];
 	omg.style.backgroundColor = "yellow";
 	iDiv.appendChild(omg);
 	document.getElementsByTagName('body')[0].appendChild(iDiv);
 	// The variable iDiv is still good... Just append to it.
 	//iDiv.appendChild(innerDiv);
-	id++;
+	
 }
 }
